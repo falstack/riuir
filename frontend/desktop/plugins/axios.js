b2ca3cbebc3b05e01fcfadadc45b87dba1784c35
falstack <icesilt@outlook.com>
2017-07-04 22:19:22 +0800

    del: axios timeout

--- frontend/desktop/plugins/axios.js
+++ frontend/desktop/plugins/axios.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: process.env.NODE_ENV !== 'production' ? 'http://riuir.dev' : 'http://api.riuir.com',
-  timeout: 10000
+  baseURL: process.env.NODE_ENV !== 'production' ? 'http://riuir.dev' : 'http://api.riuir.com'
 })
