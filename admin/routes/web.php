91c46aca3ad5db77ebb9cf4e9cb74ca755faa774
falstack <icesilt@outlook.com>
2017-07-07 21:56:42 +0800

    mod: block register

--- admin/routes/web.php
+++ admin/routes/web.php
@@ -2,13 +2,13 @@
 
 Route::get('/', 'LoginController@index');
 
-Route::get('register', 'RegisterController@showRegistrationForm')->name('register');
-Route::post('register', 'RegisterController@register');
-
 Route::get('login', 'LoginController@showLoginForm')->name('login');
 Route::post('login', 'LoginController@login');
 
 Route::group(['middleware' => ['auth']], function ()
 {
     Route::post('logout', 'LoginController@logout')->name('logout');
+
+    Route::get('register', 'RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'RegisterController@register');
 });
