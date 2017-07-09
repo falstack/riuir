0eb125ba232d16a7dd0efb9d1546a3056bfa9818
falstack <icesilt@outlook.com>
2017-07-10 07:39:48 +0800

    fix: baidu analytics.js to head

--- frontend/desktop/plugins/analytics.js
+++ frontend/desktop/plugins/analytics.js
@@ -5,21 +5,23 @@
 import VueRouter from 'vue-router'
 
 if (process.BROWSER_BUILD && process.env.NODE_ENV === 'production') {
-  const _hmt = _hmt || [];
-  (function() {
-    const hm = document.createElement('script');
-    hm.src = 'https://hm.baidu.com/hm.js?5c22f3db91001090bfc7d5096c296534';
-    const s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(hm, s);
-  })();
-  /*
-   ** 每次路由变更时进行pv统计
-   */
-  const router = new VueRouter()
-  router.afterEach((to, from) => {
+  window.onload = function () {
+    const _hmt = _hmt || [];
+    (function() {
+      const hm = document.createElement('script');
+      hm.src = 'https://hm.baidu.com/hm.js?5c22f3db91001090bfc7d5096c296534';
+      const s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(hm, s);
+    })();
     /*
-     ** 告诉 GA 增加一个 PV
+     ** 每次路由变更时进行pv统计
      */
-    _hmt.push(['_trackPageview', to.fullPath])
-  })
+    const router = new VueRouter()
+    router.afterEach((to, from) => {
+      /*
+       ** 告诉 GA 增加一个 PV
+       */
+      _hmt.push(['_trackPageview', to.fullPath])
+    })
+  }
 }
