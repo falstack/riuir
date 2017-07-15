586909a2253e7999f4e63027fd9f14fba8f7a26b
falstack <icesilt@outlook.com>
2017-07-15 22:04:46 +0800

    fix: baidu analytics router

--- frontend/desktop/plugins/analytics.js
+++ frontend/desktop/plugins/analytics.js
@@ -2,26 +2,20 @@
 /*
  ** 只在生产模式的客户端中使用
  */
-import VueRouter from 'vue-router'
-
-if (process.BROWSER_BUILD && process.env.NODE_ENV === 'production') {
-  const _hmt = _hmt || [];
-  (function() {
-    const hm = document.createElement('script');
-    hm.src = 'https://hm.baidu.com/hm.js?5c22f3db91001090bfc7d5096c296534';
-    const s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(hm, s);
-  })();
-  /*
-   ** 每次路由变更时进行pv统计
-   */
-  const router = new VueRouter({
-    mode: 'history'
-  })
-  router.afterEach((to, from) => {
+export default ({ app, store, }) => {
+  if (process.BROWSER_BUILD && process.env.NODE_ENV === 'production') {
+    let _hmt = _hmt || [];
+    (function() {
+      const hm = document.createElement('script');
+      hm.src = 'https://hm.baidu.com/hm.js?5c22f3db91001090bfc7d5096c296534';
+      const s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(hm, s);
+    })();
     /*
-     ** 告诉 GA 增加一个 PV
+     ** 每次路由变更时进行pv统计
      */
-    _hmt.push(['_trackPageview', to.fullPath])
-  })
+    app.router.afterEach((to, from) => {
+      _hmt.push(['_trackPageview', to.fullPath])
+    })
+  }
 }
