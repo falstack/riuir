fa56925231f394f4b1b164799fbe90c9ee2385c9
falstack <icesilt@outlook.com>
2017-07-19 07:40:43 +0800

    add: MySQL PDO for JSON

--- admin/config/database.php
+++ admin/config/database.php
@@ -52,6 +52,13 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
+            'options' => [
+                PDO::ATTR_CASE => PDO::CASE_LOWER,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_ORACLE_NULLS => PDO::NULL_NATURAL,
+                PDO::ATTR_STRINGIFY_FETCHES => true,
+                PDO::ATTR_EMULATE_PREPARES => true
+            ],
         ],
 
         'pgsql' => [
