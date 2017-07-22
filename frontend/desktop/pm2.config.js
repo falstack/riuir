c99c665c21d40c111fd0ab33bffb6114d319d237
falstack <icesilt@outlook.com>
2017-07-22 12:06:32 +0800

    simplify pm2

--- frontend/desktop/pm2.config.js
+++ frontend/desktop/pm2.config.js
@@ -3,6 +3,7 @@ module.exports = {
     name        : "desktop",
     script      : "./server/index.js",
     watch       : true,
+    ignore_watch: "node_modules",
     env: {
       "NODE_ENV": "development",
     },
