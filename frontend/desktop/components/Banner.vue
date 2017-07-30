5886e3fb19f242eae1a34f6dc838e9225a394938
falstack <icesilt@outlook.com>
2017-07-30 09:48:47 +0800

    add: resize width 1920px

--- frontend/desktop/components/Banner.vue
+++ frontend/desktop/components/Banner.vue
@@ -7,7 +7,7 @@
 </style>
 
 <template>
-  <div id="banner" class="bg" :style="{ 'backgroundImage': `url(${$resize('http://cdn.riuir.com/banner/1.jpg')})` }"></div>
+  <div id="banner" class="bg" :style="{ 'backgroundImage': `url(${$resize('http://cdn.riuir.com/banner/1.jpg', { width: 1920 })})` }"></div>
 </template>
 
 <script>
