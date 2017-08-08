e683fe78c010e6476a70e37101e1c0da5878b732
falstack <icesilt@outlook.com>
2017-08-08 23:14:39 +0800

    update: http -> https

--- frontend/desktop/plugins/axios.js
+++ frontend/desktop/plugins/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: process.env.NODE_ENV !== 'production' ? 'http://riuir.dev' : 'http://api.riuir.com'
+  baseURL: process.env.NODE_ENV !== 'production' ? 'http://riuir.dev' : 'https://api.riuir.com'
 })
