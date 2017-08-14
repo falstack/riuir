4bcb613f65c769f2f533af601c6317a6d519564b
falstack <icesilt@outlook.com>
2017-08-14 22:54:56 +0800

    add: navbar theme -> mask style recover

--- frontend/desktop/components/Header.vue
+++ frontend/desktop/components/Header.vue
@@ -125,28 +125,13 @@
 
     &.mask {
       .shim {
-        &:before, &:after {
-          content: '';
-          position: absolute;
-          top: 0;
-          height: 300px;
-          opacity: .5;
-          z-index: -1;
-        }
-
-        &:before {
-          left: 0;
-          width: 1200px;
-          background: linear-gradient(350deg,rgba(0,0,0,0) 0%,rgba(0,0,0,0) 59%,rgba(0,0,0,.64) 100%);
-          filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#7d000000',endColorstr='#00000000',GradientType=1);
-        }
-
-        &:after {
-          right: 0;
-          width: 800px;
-          background: linear-gradient(15deg,rgba(0,0,0,0) 0%,rgba(0,0,0,0) 59%,rgba(0,0,0,.64) 100%);
-          filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#00000000',endColorstr='#7d000000',GradientType=1);
-        }
+        width: 100%;
+        height: 100px;
+        position: absolute;
+        left: 0;
+        top: 0;
+        z-index: -1;
+        background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAABkCAYAAABOx/oaAAAAAXNSR0IArs4c6QAAAX5JREFUSA2llAtuwzAMQ5NtN9r9r9bUZP0I2VW7ATWQ2KZISv4k53Ecv+NZ2tcym5MOvDrwBLyKjZkCzvEQOH8mIGICkosBy0E8xTIwXpfkam+ZJiCvzAPQHrzIztxyEnkyI+0yW8/s0pJdngKybvlS0sLsQK99l3s/ZYPcPYkUoKUkAPd17ZJa3mbfwTA9QKq+y+5lVqbHbHINHDuoOnOXxIRt5m0A3BCNW7nByKYinlIp2MqdUCU5KupsuQwA6r12SiJhmABhLsBAfZoVbJkCn1b0IegruR+cKulPc2d+mF2JvEvOaLPHa8nOxmSX6neU6805mU2di69uMixy3WAKWBJVYPFEakKVC/CVBKROy/fvyDYwPeElJqXQP13vlyUtXxyW3k9N4qdJTaS52mvmI14s/pQnGUwBbEp+gM46zXOakSrwPR7JIhUoT7WF2YEXUuTq7QU7Nkr01CorQYE1s8c2HgF6sdtA/gxhaEBDTm98mcCsvQn/Y1XZ+/EdtDiR1Lr0wCcAAAAASUVORK5CYII=);
       }
     }
 
