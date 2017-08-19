af68e15eaa740d54dea5f1595381ecac3117676b
falstack <icesilt@outlook.com>
2017-08-19 22:39:15 +0800

    fix

--- frontend/desktop/plugins/axios.js
+++ frontend/desktop/plugins/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 import Vue from 'vue'
 
 export default axios.create({
-  baseURL: process.env.NODE_ENV !== 'production' ? 'http://riuir.dev/' : Vue.prototype.$isServer ? '127.0.0.1:443/' : 'https://api.riuir.com/'
+  baseURL: process.env.NODE_ENV !== 'production' ? 'http://riuir.dev/' : Vue.prototype.$isServer ? 'https://172.19.36.16:443/' : 'https://api.riuir.com/'
 })
