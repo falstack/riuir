c3964f159583ac296f00437a8df2bb1e3d2d2b5e
falstack <icesilt@outlook.com>
2017-08-19 22:43:33 +0800

    fix

--- frontend/desktop/plugins/axios.js
+++ frontend/desktop/plugins/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import Vue from 'vue'
+import host from '../.env'
 
 export default axios.create({
-  baseURL: process.env.NODE_ENV !== 'production' ? 'http://riuir.dev/' : Vue.prototype.$isServer ? 'https://172.19.36.16:443/' : 'https://api.riuir.com/'
+  baseURL: process.env.NODE_ENV !== 'production' ? 'http://riuir.dev/' : Vue.prototype.$isServer ? `${host}/` : 'https://api.riuir.com/'
 })
