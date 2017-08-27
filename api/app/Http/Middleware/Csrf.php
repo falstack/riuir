d0f6ff69a495b0214e089a56defd39dd932a8cfa
falstack <icesilt@outlook.com>
2017-08-27 10:57:07 +0800

    add: maybe fixed csrf

--- api/app/Http/Middleware/Csrf.php
+++ api/app/Http/Middleware/Csrf.php
@@ -15,9 +15,6 @@ class Csrf
     public function handle($request, Closure $next)
     {
         $origin = $request->headers->get('Origin');
-        \Log::info($request->ip());
-        \Log::info($origin);
-        \Log::info($request);
         if (
             in_array($origin, $this->allows) ||
             empty($origin) && $request->ip() === '127.0.0.1'
