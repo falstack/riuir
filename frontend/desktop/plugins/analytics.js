72bfaa31a064b86ce939dc615c8a0bdfa884e521
falstack <icesilt@outlook.com>
2017-08-31 20:27:45 +0800

    fixed: baidu stat++

--- frontend/desktop/plugins/analytics.js
+++ frontend/desktop/plugins/analytics.js
@@ -1,23 +1,18 @@
-/*
- ** 只在生产模式的客户端中使用
- */
 export default ({ app }) => {
-  if (process.BROWSER_BUILD && process.env.NODE_ENV === 'production') {
-    (function () {
-      const s = document.getElementsByTagName('script')[0]
-      /* 360统计 */
-      const qh = document.createElement('script')
-      qh.async = true
-      qh.defer = true
-      qh.src = '//s.union.360.cn/174447.js'
-      s.parentNode.insertBefore(qh, s)
-    })()
-    /*
-     ** 每次路由变更时进行pv统计
-     */
-    app.router.afterEach((to) => {
-      // eslint-disable-next-line
-      _hmt.push(['_trackPageview', to.fullPath])
-    })
-  }
+  (function () {
+    const s = document.getElementsByTagName('script')[0]
+    /* 360统计 */
+    const qh = document.createElement('script')
+    qh.async = true
+    qh.defer = true
+    qh.src = '//s.union.360.cn/174447.js'
+    s.parentNode.insertBefore(qh, s)
+  })()
+  /*
+   ** 每次路由变更时进行pv统计
+   */
+  app.router.afterEach((to) => {
+    // eslint-disable-next-line
+    _hmt.push(['_trackPageview', to.fullPath])
+  })
 }
