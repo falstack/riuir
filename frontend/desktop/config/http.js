c93adc309f2cc74e1bda9fe73badf5152c844964
falstack <icesilt@outlook.com>
2017-09-03 10:57:14 +0800

    ...

--- frontend/desktop/config/http.js
+++ frontend/desktop/config/http.js
@@ -1,7 +1,7 @@
 const host = require('../.env').host
 
 exports.config = {
-  baseURL: process.env.NODE_ENV !== 'production' ? host.dev : typeof window === 'undefined' ? '/' : host.prod,
+  baseURL: process.env.NODE_ENV === 'development' ? host.dev : typeof window === 'undefined' ? '/' : host.prod,
   headers: { Accept: 'application/json' },
   withCredentials: true,
   timeout: 10000,
